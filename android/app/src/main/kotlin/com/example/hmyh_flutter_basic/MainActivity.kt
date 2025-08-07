a4a7daffba420589bfa5bc49668938da980f375c
YOMA\hsumyatyehtet <hsumyatyehtet@onenex.co>
2025-08-07 22:32:36 +0630

    project set up

---
+++ android/app/src/main/kotlin/com/example/hmyh_flutter_basic/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hmyh_flutter_basic
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/basic_flutter_app.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+            child: Text(
+                'Hello World!',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue
+                )
+            )
+        ),
+      ),
+    );
+  }
+}
