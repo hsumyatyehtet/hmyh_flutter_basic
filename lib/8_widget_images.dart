f451b29e70da02faef2aee74c79d24b8234d832f
YOMA\hsumyatyehtet <hsumyatyehtet@onenex.co>
2025-08-22 16:21:10 +0630

    image height change

--- lib/8_widget_images.dart
+++ lib/8_widget_images.dart
@@ -69,7 +69,7 @@ class ImageExample extends StatelessWidget {
               borderRadius: BorderRadius.circular(12),
               child: Image.network(
                 'https://images.unsplash.com/photo-1695048132832-b41495f12eb4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-                height: 150,
+                height: 180,
                 width: double.infinity,
                 fit: BoxFit.cover,
               ),
