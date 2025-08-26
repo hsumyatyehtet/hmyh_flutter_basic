33b3d9dc27aef3a2e651b81b0b297cb2c90990e3
YOMA\hsumyatyehtet <hsumyatyehtet@onenex.co>
2025-08-26 11:15:17 +0630

    layout builder advance

---
+++ lib/11_widget_layout_builder_advance.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(home: LayoutBuilderAdvance()));
+}
+
+class LayoutBuilderAdvance extends StatelessWidget {
+  final List<Color> colors = [
+    Colors.red,
+    Colors.green,
+    Colors.blue,
+    Colors.orange,
+    Colors.purple,
+    Colors.teal,
+    Colors.pink,
+    Colors.indigo,
+  ];
+
+  LayoutBuilderAdvance({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Advance LayoutBuilder')),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          int columns = (constraints.maxWidth / 150).floor();
+          columns = columns < 1 ? 1 : columns;
+
+          return GridView.builder(
+            padding: const EdgeInsets.all(16),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: columns,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              childAspectRatio: 1,
+            ),
+            itemCount: colors.length,
+            itemBuilder: (context, index) {
+              return Container(
+                decoration: BoxDecoration(
+                  color: colors[index],
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Center(
+                  child: Text(
+                    'Item $index\n$columns cols',
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
